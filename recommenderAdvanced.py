c934d6c66f1b58519fe14d9e78f4ceeb4dbbf02b
theNann <panyanan1211@163.com>
2017-12-27 16:50:20 +0800

    对recommenderAdvanced添加第二种冷启动方法coldld_start_advanced

--- recommenderAdvanced.py
+++ recommenderAdvanced.py
@@ -1,25 +1,59 @@
 import numpy as np
+import math
 from sklearn.neighbors import NearestNeighbors
 import prepareData
 import csv
 import myparser
 import knn
+Fov = 1.0472
 
 
-def cold_start(data_train_p, data_test_p, data_train_d, data_test_d, type, neighbors):
-    dists, inds = knn.get_nearest_neighbors(data_train_p, data_test_p, neighbors, type)
+# find n neighbors according to 'position'
+# return inds, whose shape is(1L, nL)
+def cold_start(data_train_p, data_test_p, neighbors):
+    dists, inds = knn.get_nearest_neighbors(data_train_p, data_test_p, neighbors, type='position')
     return inds
 
 
+# find n neighbors according to 'position', and then delete neighbor which cos<d1,d2> > cos(fov)
+# return inds, whose shape is(1L, nL)
+def cold_start_advanced(data_train_p, data_test_p, data_train_d, data_test_d, neighbors, sub_neighbors):
+    global Fov
+    d_test = data_test_d[0]
+    dits, inds = knn.get_nearest_neighbors(data_train_p, data_test_p, neighbors, type='position')
+    coss = {}
+    for index in inds[0]:
+        d_train = data_train_d[index]
+        cos, sim = myparser.cal_vector_similarity(np.mat(d_test), np.mat(d_train))
+        coss[index] = cos
+    sort_coss = sorted(coss.items(), key=lambda item: item[1], reverse=True)
+    print(sort_coss)
+    ans_inds = []
+    for cos in sort_coss:
+        if cos < math.cos(Fov):
+            break
+        else:
+            ans_inds.append(cos[0])
+        if len(ans_inds) == sub_neighbors:
+            break
+    if len(ans_inds) == 0:
+        ans_inds.append(inds[0][0])
+    return np.array([ans_inds])
+
+
 def user_based_recommend(data_train_p, data_test_p, data_train_d, data_test_d, target_train, target_test, neighbors,
-                         bound):
+                         sub_neighbors, bound):
     scores = []
     for data_test_id in range(len(data_test_p)):
+    # for data_test_id in range(5, 6, 1):
         print(neighbors, data_test_id)
         # cold start
-        inds = cold_start(data_train_p[:, 1:4], np.array([data_test_p[data_test_id, 1:4]]), data_train_d[:, 1:4],
-                          np.array([data_test_d[data_test_id, 1:4]]), type='position', neighbors=neighbors)
+        # inds = cold_start(data_train_p[:, 1:4], np.array([data_test_p[data_test_id, 1:4]]), neighbors=neighbors)
+        inds = cold_start_advanced(data_train_p[:, 1:4], np.array([data_test_p[data_test_id, 1:4]]),
+                                   data_train_d[:, 1:4], np.array([data_test_d[data_test_id, 1:4]]),
+                                   neighbors=neighbors, sub_neighbors=sub_neighbors)
         # print("inds: ", inds)
+        # print(inds.shape)
         set_total_predict_list = set()
         for i in range(len(inds[0])):
             predict_list = target_train[inds[0][i]][1:]
@@ -56,15 +90,16 @@ def user_based_recommend(data_train_p, data_test_p, data_train_d, data_test_d, t
 
 def main():
     data_train_p, data_test_p, data_train_d, data_test_d, target_train, target_test = prepareData.prepare_data()
-    neighbors_list = [3, 5]
+    neighbors_list = [1, 2, 3, 4, 5, 6]
     score_recommend_by_neighbors = []
     for neighbors in neighbors_list:
         mean = user_based_recommend(data_train_p, data_test_p, data_train_d, data_test_d, target_train, target_test,
-                                    neighbors, 0.99)
+                                    neighbors=15, sub_neighbors=neighbors, bound=0.99)
+        print(mean[0], mean[1])
         score_recommend_by_neighbors.append([neighbors, mean[0], mean[1]])
     csv_file = open('output/tmp.csv', 'ab')
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['neighbors', 'sim_acc', 'sim_recall'])
+    csv_writer.writerow(['sub_neighbors', 'sim_acc', 'sim_recall'])
     csv_writer.writerows(score_recommend_by_neighbors)
     csv_file.close()
     pass
