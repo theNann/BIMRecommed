fa42f6c817e98870ac50eb4ed2920b0c53ce1e84
theNann <panyanan1211@163.com>
2018-06-04 21:11:05 +0800

    可以将外部数据添加到测试数据集合中，实现随意扩充测试集

--- myparser.py
+++ myparser.py
@@ -2,6 +2,7 @@ import string
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 DATA_ID = -1
 
@@ -132,7 +133,7 @@ def read_data_txt(file_paths):
 def solve_data():
     # file_paths = ["input/DataSet/input1.txt", "input/DataSet/input2.txt", "input/DataSet/input3.txt",
     #               "input/DataSet/input4.txt", "input/DataSet/input5.txt"]
-    file_paths = ["E:/DataSet/test1.txt"]
+    file_paths = ["/home/pyn/Desktop/DataSet/test5.txt"]
     data, target = read_data_txt(file_paths)
     print(data.shape)
     print(target.shape)
@@ -148,13 +149,13 @@ def solve_data():
     # print(target_train.shape, target_test.shape)
 
     # write csv file
-    csv_file = open('E:/DataSet/data1.csv', 'wb')
+    csv_file = open('/home/pyn/Desktop/DataSet/data5.csv', 'wb')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['data_id', 'p1', 'p2', 'p3', 'd1', 'd2', 'd3', 'u1', 'u2', 'u3'])
     csv_writer.writerows(data)
     csv_file.close()
 
-    txt_file = open('E:/DataSet/target1.txt', 'wb')
+    txt_file = open('/home/pyn/Desktop/DataSet/target5.txt', 'wb')
     for target_train_i in target:
         tmp = str(target_train_i)
         tmp = tmp[1:len(tmp) - 1]
@@ -193,6 +194,50 @@ def solve_data():
     # txt_file.close()
 
 
+def expandTestData():
+    data_csv_files = ["/home/pyn/Desktop/DataSet/data1.csv", "/home/pyn/Desktop/DataSet/data2.csv",
+                      "/home/pyn/Desktop/DataSet/data3.csv", "/home/pyn/Desktop/DataSet/data4.csv",
+                      "/home/pyn/Desktop/DataSet/data5.csv"]
+    write_data_file = "/home/pyn/Desktop/BIMRecommed/input/data_test.csv"
+
+    for file in data_csv_files:
+        df = pd.read_csv(file)
+        df.to_csv(write_data_file, encoding="utf_8", index=False, header=False, mode='a+')
+    print("write finish")
+
+    # target_txt_files = ['/home/pyn/Desktop/DataSet/target1.txt', '/home/pyn/Desktop/DataSet/target2.txt',
+    #                     '/home/pyn/Desktop/DataSet/target3.txt', '/home/pyn/Desktop/DataSet/target4.txt',
+    #                     '/home/pyn/Desktop/DataSet/target5.txt']
+    # target = read_target_test(target_txt_files)
+    # txt_file = open('/home/pyn/Desktop/BIMRecommed/input/target_test.txt', 'a')
+    # index = 2368
+    # for target_i in target:
+    #     target_i[0] = index
+    #     tmp = str(target_i)
+    #     tmp = tmp[1:len(tmp) - 1]
+    #     txt_file.writelines(tmp + '\n')
+    #     index += 1
+    # txt_file.close()
+    # print("write finish")
+
+
+def read_target_test(target_txt_files):
+    target = []
+    for file_path in target_txt_files:
+        file_object = open(file_path)
+        file_lines = file_object.readlines()
+        for line in file_lines:
+            data_str = line[0:len(line)-1].strip()
+            # print(data_str)
+            if len(data_str) > 0:
+                data_list = data_str.split(", ")
+                data = []
+                for d in data_list:
+                    data.append(int(d))
+                target.append(data)
+    return target
+
+
 def solve_test():
     file_paths = ["input/DataSet/test.txt"]
     queries_by_pdu, results = read_data_txt(file_paths)
@@ -214,5 +259,6 @@ def solve_test():
 
 
 if __name__ == "__main__":
-    solve_data()
+    # solve_data()
     # solve_test()
+    expandTestData()
